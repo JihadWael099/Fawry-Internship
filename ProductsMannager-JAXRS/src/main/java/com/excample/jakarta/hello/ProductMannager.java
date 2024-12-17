8e71c86a4110b5cef5c715d752e84e10531e6b8a
jihad wael <144389076+JihadWael099@users.noreply.github.com>
2024-12-17 15:07:58 +0200

    Update ProductMannager.java

--- ProductsMannager-JAXRS/src/main/java/com/excample/jakarta/hello/ProductMannager.java
+++ ProductsMannager-JAXRS/src/main/java/com/excample/jakarta/hello/ProductMannager.java
@@ -7,14 +7,7 @@ public class ProductMannager {
 
     private static  List<Product> productList = new ArrayList<>();
 
-    static {
-        // Adding sample products to the list
-        productList.add(new Product("Product 1", 19.99, 1));
-        productList.add(new Product("Product 2", 29.99, 2));
-        productList.add(new Product("Product 3", 39.99, 3));
-        productList.add(new Product("Product 4", 49.99, 4));
-        productList.add(new Product("Product 5", 59.99, 5));
-    }
+
     public void AddProduct(Product product){
         productList.add(product);
     }
